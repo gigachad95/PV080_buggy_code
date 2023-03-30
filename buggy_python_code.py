0bde133e5e2c23225f2685317817c441c2b2bb21
unknown <xvalko3@ad.fi.muni.cz>
2023-03-30 16:43:55 +0200

    fix: fixed some issues

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,5 +1,3 @@
-import sys 
-import os
 import yaml
 import flask
 
@@ -37,7 +35,7 @@ def fetch_website(urllib_version, url):
 
 def load_yaml(filename):
     stream = open(filename)
-    deserialized_data = yaml.load(stream, Loader=yaml.Loader) #deserializing data
+    deserialized_data = yaml.safe_load(stream, Loader=yaml.Loader) #deserializing data
     return deserialized_data
     
 def authenticate(password):
